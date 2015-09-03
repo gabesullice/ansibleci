ebdb43bdb860c86dcbe4abb182f1f93ef644ac01
Gabriel SULLICE <gabriel@sullice.com>
2015-09-02 18:29:54 -0600

    Changes

--- src/ansibleci.go
+++ src/ansibleci.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	//"os/exec"
+	"os/exec"
 )
 
 func main() {
@@ -19,6 +19,8 @@ func main() {
 
 func DeployFunc(w http.ResponseWriter, r *http.Request) {
 	if err := os.Chdir("/app/deploykit-master"); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		r.Body.Close()
 		log.Fatal(err)
 	}
 
@@ -26,16 +28,29 @@ func DeployFunc(w http.ResponseWriter, r *http.Request) {
 	var b map[string]interface{}
 	if err := decoder.Decode(&b); err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		r.Body.Close()
 		return
 	}
 
-	log.Println(b)
+	log.Println(r.Body)
 
-	//os.Setenv("ANSIBLE_SSH_ARGS", "-o \"StrictHostKeyChecking no\" -o \"ForwardAgent yes\"")
-	//cmd := exec.Command("make", "deploy.dev")
-	//cmd.Stderr = os.Stderr
-	//cmd.Stdout = os.Stdout
-	//if err := cmd.Run(); err != nil {
-	//	log.Println(err)
-	//}
+	go func() {
+		os.Setenv("ANSIBLE_SSH_ARGS", "-o \"StrictHostKeyChecking no\" -o \"ForwardAgent yes\"")
+		cmd := exec.Command("make", "deploy.dev")
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		if err := cmd.Start(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			r.Body.Close()
+			log.Println(err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	w.WriteHeader(http.StatusOK)
+	r.Body.Close()
 }
